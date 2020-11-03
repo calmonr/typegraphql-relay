94cce3ee9f92324e7c9a720fb6b5e9063bad4fd4
Calmon Ribeiro <calmonrib@gmail.com>
2020-11-03 08:38:32 -0300

    fix: edges should be nullable for items and list

--- src/decorators/relay/connection.type.ts
+++ src/decorators/relay/connection.type.ts
@@ -11,7 +11,10 @@ export function ConnectionType<V extends ClassType, T extends ClassType>(
       description: 'A connection to a list of items.'
     })
     class ConnectionType extends target implements Connection<V> {
-      @Field(() => [EdgeType], { description: 'A list of edges.' })
+      @Field(() => [EdgeType], {
+        description: 'A list of edges.',
+        nullable: 'itemsAndList'
+      })
       readonly edges!: Array<Edge<V>>
 
       @Field({ description: 'Information to aid in pagination.' })
