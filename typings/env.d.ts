edfa8ab80ebd8790de02a9af3d43fb7e02d8fd2b
Calmon Ribeiro <calmonrib@gmail.com>
2020-11-02 22:17:15 -0300

    feat: docker-compose and postgres service

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -1,5 +1,10 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     APPLICATION_NAME: string
+    DATABASE_HOST: string
+    DATABASE_PORT: number
+    DATABASE_USERNAME: string
+    DATABASE_PASSWORD: string
+    DATABASE_NAME: string
   }
 }
